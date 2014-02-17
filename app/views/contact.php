89fc68a11cd86a237757786a949e387333979bae
Santiago Bernal B <exactlimon@gmail.com>
2014-02-17 16:33:25 -0500

    1

--- app/views/contact.php
+++ app/views/contact.php
@@ -1,9 +1,15 @@
 <html>
     <body>
         <form action="/contact" method="POST">
-
-            Your form
-
+        <LABEL for="name">Nombre: </LABEL>
+              <INPUT type="text" id="nombre"><BR>
+    	<!-- <LABEL for="surname">Apellido: </LABEL>
+              <INPUT type="text" id="surname"><BR> -->
+    	<LABEL for="email">email: </LABEL>
+              <INPUT type="text" id="email"><BR>
+        <LABEL for="subjetc">asunto: </LABEL>
+              <INPUT type="textarea" id="subjetc"><BR> 
+              <INPUT type="submit" value="Enviar">     
         </form>
     </body>
 </html>
