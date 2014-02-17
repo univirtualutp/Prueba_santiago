8571292d564d2f2e780f2630a4854232c7bdc765
Santiago Bernal B <exactlimon@gmail.com>
2014-02-17 15:57:14 -0500

    hola

--- app/views/formulario.php
+++ app/views/formulario.php
@@ -1,4 +1,4 @@
-<<!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -49,8 +49,35 @@
     </select>
   </div>
   <!-- Mas campos aqui… -->
-  {{ Form::label('full_name', 'Nombre completo') }}
 </form>
+
+<h2>aqui otro form</h2>
+{{ Form::open(array('method' => 'POST'), array('role' => 'form')) }}
+
+  <div class="row">
+    <div class="form-group col-md-4">
+      {{ Form::label('email', 'Dirección de E-mail') }}
+      {{ Form::text('email', null, array('placeholder' => 'Introduce tu E-mail', 'class' => 'form-control')) }}
+    </div>
+    <div class="form-group col-md-4">
+      {{ Form::label('full_name', 'Nombre completo') }}
+      {{ Form::text('full_name', null, array('placeholder' => 'Introduce tu nombre y apellido', 'class' => 'form-control')) }}        
+    </div>
+  </div>
+  <div class="row">
+    <div class="form-group col-md-4">
+      {{ Form::label('password', 'Contraseña') }}
+      {{ Form::password('password', array('class' => 'form-control')) }}
+    </div>
+    <div class="form-group col-md-4">
+      {{ Form::label('password_confirmation', 'Confirmar contraseña') }}
+      {{ Form::password('password_confirmation', array('class' => 'form-control')) }}
+    </div>
+  </div>
+  {{ Form::button('Crear usuario', array('type' => 'submit', 'class' => 'btn btn-primary')) }}    
+  
+{{ Form::close() }}
+
 	</div>
 </body>
 </html>
