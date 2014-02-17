2ba06499428735ed746e4b0bd1a392c7092233a7
Santiago Bernal B <exactlimon@gmail.com>
2014-02-17 16:49:03 -0500

    1

--- app/views/form1.php
+++ app/views/form1.php
@@ -1,4 +1,12 @@
 <h1>formulario de reporte</h1>
 <?php
 echo Form::open(array('url' => 'foo/bar', 'files' => true));
+echo Form::token();
+echo Form::text('nombre');
+echo Form::text('apellidos');
+echo Form::label('email', 'E-Mail Address');
+echo Form::text('asunto');
+echo Form::text('descripcion');
+echo Form::file('image');
+echo Form::submit('enviar!');
 ?>
